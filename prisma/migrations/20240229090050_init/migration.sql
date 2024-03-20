d5d6e6d9e8656658c66217e9de733d8244df4f9f
EsMagicoKunal <kunal@esmagico.in>
2024-03-20 17:09:50 +0530

    update

--- prisma/migrations/20240229090050_init/migration.sql
+++ prisma/migrations/20240229090050_init/migration.sql
@@ -1,6 +1,3 @@
--- CreateExtension
-CREATE EXTENSION IF NOT EXISTS "vector";
-
 -- CreateTable
 CREATE TABLE "speech_to_text" (
     "id" SERIAL NOT NULL,
@@ -16,3 +13,16 @@ CREATE TABLE "speech_to_text" (
 
     CONSTRAINT "speech_to_text_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateTable
+CREATE TABLE "Metrics" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+
+    CONSTRAINT "Metrics_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Metrics_name_key" ON "Metrics"("name");
